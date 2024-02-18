9966ee7a29044e326504e4394bb265cd6691e16e
cschavhan <chandrakantchavhan01@gmail.com>
2024-02-18 22:16:33 +0530

    add show more functionality to the posts results of the dashboard

--- Client/src/components/DashPosts.jsx
+++ Client/src/components/DashPosts.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 
 function DashPosts() {
   const [userPost, setUserPost] = useState([]);
+  const [showMore, setShowMore] = useState(true);
   const { currentUser } = useSelector((state) => state.user);
 
   useEffect(() => {
@@ -14,6 +15,9 @@ function DashPosts() {
         const data = await res.json();
         if (res.ok) {
           setUserPost(data.posts);
+          if (data.posts.length < 9) {
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -24,6 +28,25 @@ function DashPosts() {
       fetchPosts();
     }
   }, [currentUser._id]);
+
+  const handleShowMore = async () => {
+    const startIndex = userPost.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+
+      const data = await res.json();
+      if (res.ok) {
+        setUserPost((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto w-full p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
@@ -90,6 +113,15 @@ function DashPosts() {
               </Table.Body>
             ))}
           </Table>
+
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className="w-full text-teal-500 self-center text-sm py-7"
+            >
+              Show More
+            </button>
+          )}
         </>
       ) : (
         <p>You dont have any post yet</p>
